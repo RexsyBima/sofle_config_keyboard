8c0e6371518c29d25a2db102900f091ccbab77d5
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-03-26 04:09:50 +0700

    added some config.h settings

--- keymaps/rexsy/config.h
+++ keymaps/rexsy/config.h
@@ -8,13 +8,18 @@
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
-#define TAPPING_TERM 150
+#define TAPPING_TERM 175
+#define QUICK_TAP_TERM 0
 // #define RETRO_TAPPING
 //
 //
 // #define SPLIT_WPM_ENABLE
 // Disabled to save space
 #define NO_ACTION_ONESHOT  // 332
+#define FORCE_NKRO  // 332
 #define NO_ACTION_MACRO    // 0
 #define NO_ACTION_FUNCTION // 0
 #define DISABLE_LEADER     // 0
+//
+#define SPLIT_USB_TIMEOUT 10000
+#define SPLIT_USB_TIMEOUT_POLL 10
