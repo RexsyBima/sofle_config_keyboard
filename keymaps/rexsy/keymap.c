bfb2922ccb86c5dc4b10edc85f4ba10a3c4cbc26
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-03-30 08:17:04 +0700

    changes to last layer

--- keymaps/rexsy/keymap.c
+++ keymaps/rexsy/keymap.c
@@ -113,32 +113,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  _______,  KC_NUM,  KC_P1,   KC_P2,  KC_P3, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
+  _______,  KC_NUM,  KC_P7, KC_P8, KC_P9, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
   _______, KC_LALT,  KC_P4,  KC_P5,  KC_P6, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______, C(KC_Z), KC_P7, KC_P8, KC_P9, KC_P0,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
+  _______, C(KC_Z), KC_P1,   KC_P2,  KC_P3, KC_P0,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
-/* ADJUST
+
+/*
+ * COLEMAK
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |`esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | QK_BOOT|      ||COLEMAK|      |      |                    |      |      |      |      |      |      |
+ * | TAB  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  | "'   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |MACWIN|      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
+ * |ESCSFT| ALGUI| RLALT| SLSFT|TLCTL |   G  |-------.    ,-------|   M  | NLGUI| ELALT| ILSFT|OLCTL |  =+  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
+ * |CW_TOG|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,< | .>   | \|   |  /?  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |TG_AJST|XXXXXXX| KC_LALT |LOWER |/Spcjst/\Space \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
   [_ADJUST] = LAYOUT(
-  XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  QK_BOOT  , XXXXXXX,XXXXXXX,KC_COLEMAK,CG_TOGG,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX,CG_TOGG, XXXXXXX,    XXXXXXX,  XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
-                   TG_AJST, _______, _______, _______, _______,     _______, _______, _______, _______, _______
-  )
+  QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_QUOT,
+  ESC_SFT,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                   KC_M,    N_LCTL,    E_LSFT,    I_LALT,    O_LGUI,  KC_EQL,
+  CW_TOGG,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,      KC_CAPS,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_BSLS,  KC_SLSH,
+                 TG_AJST,XXXXXXX,KC_LALT,MO_LOWR, SPC_AST,        SC_SENT,  MO_RAIS, KC_BSPC, KC_LBRC, KC_RBRC),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
